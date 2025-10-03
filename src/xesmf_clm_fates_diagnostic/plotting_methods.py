c7ed1f1b79cfeca137084fc57eba7e82f9fdbc38
Rosie Fisher <rosief@login2-nird-lmd.nird.login.svc.cluster.local>
2025-10-03 15:54:20 +0200

    working 3D and 2D plots

--- src/xesmf_clm_fates_diagnostic/plotting_methods.py
+++ src/xesmf_clm_fates_diagnostic/plotting_methods.py
@@ -5,9 +5,48 @@ import xarray as xr
 import math
 import xesmf
 from matplotlib.colors import LogNorm
-
 from  .misc_help_functions import get_unit_conversion_and_new_label
 
+def make_3D_plot(bias,figname,yminv,ymaxv):
+
+    print("making 3d fig")
+    images = []
+    dims = list(bias.dims)
+    print("dims",dims)
+    extra_dim = [d for d in dims if d not in ["lat", "lon"]][0]
+    print("extra dim",extra_dim)
+    n = bias.sizes[extra_dim]
+    labels = [f"{extra_dim}={i}" for i in range(n)]
+    print("n",n)
+    ncols = math.ceil(math.sqrt(n))
+    nrows = math.ceil(n / ncols)
+    fig, axs = plt.subplots(nrows, ncols, figsize=(4*ncols, 3*nrows), constrained_layout=True)
+    axs = axs.flatten()
+    ims = []
+    fs=fig.suptitle(figname.split("/")[-1])
+    cfs = fs.get_fontsize()
+    fs.set_fontsize(cfs * 1.3)
+    plotted_axes = []
+    for i, ax in enumerate(axs, start=0):
+        if i < n:       
+            im =bias.isel({extra_dim: i}).plot.pcolormesh(ax=ax,vmin=yminv,vmax=ymaxv,add_colorbar=False,cmap="viridis")
+            current_fs = ax.title.get_fontsize()   # get current font size
+            ax.set_title(labels[i], fontsize=current_fs * 1.5)
+            ax.set_xlabel('')
+            ax.set_xticks([])
+            ax.set_ylabel('')
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
+            ims.append(im)
+            plotted_axes.append(ax)
+        else:
+            fig.delaxes(ax)
+    cbar=fig.colorbar(ims[0], ax=plotted_axes,location="bottom",fraction=0.04, pad=0.04)
+    cbar.ax.tick_params(labelsize=16)             
+    fignamefull=figname+'.png'
+    fig.savefig(fignamefull,bbox_inches='tight')
+
+                    
 def make_bias_plot(bias,figname,yminv=None,ymaxv=None,cmap = 'viridis',ax = None, xlabel=None, logscale=False):
     # Use viridis for absolute maps
 
@@ -18,98 +57,62 @@ def make_bias_plot(bias,figname,yminv=None,ymaxv=None,cmap = 'viridis',ax = None
     else:
         shrink = 0.5
 
-    if ax is None:  # this is a single plot situation
-
-        dims = list(bias.dims)
-        if(len(dims) == 3): # we have an extra dimension
-            # Find the "extra" dim (not lat/lon)
-            extra_dim = [d for d in dims if d not in ["lat", "lon"]][0]
-            n = bias.sizes[extra_dim]
-            print("n",n)
-            print(bias[extra_dim])
-
-            # Try to get human-readable labels (if available)
-            if n>0: #extra_dim in bias.coords:
-#                labels = [str(l) for l in bias[extra_dim].values]
-#            else:
-                labels = [f"{extra_dim}={i}" for i in range(n)]
-                ncols = math.ceil(math.sqrt(n))
-                nrows = math.ceil(n / ncols)
-                print("making 3d fig")
-                fig, axes = plt.subplots(nrows, ncols, figsize=(4*ncols, 3*nrows), constrained_layout=True)
-                images = []
-                for i in range(n):
-                    print("plotting pfts",i)
-                    ax = axes[i // ncols, i % ncols]
-                    im =bias.isel({extra_dim: i}).plot(ax=ax,vmin=yminv,vmax=ymaxv,add_colorbar=False)
-                    ax.set_title(labels[i])
-
-                    ax.set_xlabel('')
-                    ax.set_xticks([])
-                    ax.set_ylabel('')
-                    ax.set_xticklabels([])
-                    ax.set_yticklabels([])
-                    images.append(im)    
-
-                #fig.colorbar(im, ax=axes, location='right', label=bias.name)
-                    
-                # cbar_ax = fig.add_axes([0.92, 0.15, 0.02, 0.7])
-                # fig.colorbar(im, cax=cbar_ax, label=bias.name)
-
-                # Hide unused axes
-                for j in range(i+1, nrows*ncols):
-                    fig.delaxes(axes[j // ncols, j % ncols])
-
-                fig.suptitle(f"{bias.name}", fontsize=16)
-                plt.tight_layout()
-
-        else: # normal 2D plot.     
+    dims = list(bias.dims)
+    if(len(dims) == 3):
+        bias_2d_plot=bias.sum(dim=bias.dims[2])
+    else:
+        bias_2d_plot = bias
+    
+    if(len(dims) == 3 and ax is None): # we have an extra dimension
+            make_3D_plot(bias,figname,yminv,ymaxv)
+    else:         
+        if ax is None:
+            print_to_file = True
             fig = plt.figure(figsize=(10, 5))
-            # Create a GeoAxes with the PlateCarree projection
-            #ax = plt.axes(projection=ccrs.PlateCarree())
-        
             ax = plt.axes(projection=ccrs.Robinson())
             print_to_file = True
             shrink = 0.7
-    #print(figname)
-    # Plot the data on the map
+        else:
+            shrink = 0.5
 
-            if xlabel is not None:
-                shift, xlabel = get_unit_conversion_and_new_label(xlabel.split("[")[-1][:-1])
-                bias = bias + shift
-            try:
-                if (yminv is None) or (ymaxv is None):
-                    if not logscale:
-                        im = bias.plot(ax=ax, transform=ccrs.PlateCarree(),cmap=cmap)
-                    else:
-                        bias = bias.where(bias > 0)
-                        im = bias.plot(ax=ax, transform=ccrs.PlateCarree(),cmap=cmap, norm = LogNorm())
+        if xlabel is not None:
+            shift, xlabel = get_unit_conversion_and_new_label(xlabel.split("[")[-1][:-1])
+            bias_2d_plot = bias_2d_plot + shift
+
+        try:
+            if (yminv is None) or (ymaxv is None):
+                if not logscale:
+                    im = bias_2d_plot.plot(ax=ax, transform=ccrs.PlateCarree(),cmap=cmap)
                 else:
-                    im = bias.plot(ax=ax, transform=ccrs.PlateCarree(),cmap=cmap, vmin=yminv, vmax=ymaxv)
-                cb =  im.colorbar
-                cb.remove()
-                plt.colorbar(im, ax=ax, shrink=shrink)#fraction=0.046, pad=0.04
-            except TypeError as err:
-                print(f"Not able to produce plot due to {err}")
-                ax.clear()
-            
-            ax.set_title('')
-            ax.set_title(figname.split("/")[-1])
-
-            if xlabel is None:
-                ax.set_xlabel('')
+                    bias_2d_plot = bias_2d_plot.where(bias_2d_plot > 0)
+                    im = bias_2d_plot.plot(ax=ax, transform=ccrs.PlateCarree(),cmap=cmap, norm = LogNorm())
             else:
-                ax.set_xticks([])
-                ax.set_xlabel(xlabel)
-            ax.set_ylabel('')
-            ax.set_xticklabels([])
-            ax.set_yticklabels([])
-            ax.coastlines()
+                im = bias_2d_plot.plot(ax=ax, transform=ccrs.PlateCarree(),cmap=cmap, vmin=yminv, vmax=ymaxv)
+            cb =  im.colorbar
+            cb.remove()
+            plt.colorbar(im, ax=ax, shrink=shrink)#fraction=0.046, pad=0.04
+
+        except TypeError as err:
+            print(f"Not able to produce plot due to {err}")
+            ax.clear()
+            
+        ax.set_title('')
+        ax.set_title(figname.split("/")[-1])
+
+        if xlabel is None:
+            ax.set_xlabel('')
+        else:
+            ax.set_xticks([])
+            ax.set_xlabel(xlabel)
+        ax.set_ylabel('')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.coastlines()
     
-    # Show the plot
-    if print_to_file:
-        fignamefull=figname+'.png'
-        fig.savefig(fignamefull,bbox_inches='tight')
+    # Save 2D plot. 
+        if print_to_file:
+            fignamefull=figname+'.png'
+            fig.savefig(fignamefull,bbox_inches='tight')
 
 def make_bias_plot_latixy_longxy(bias,latixy, longxy, figname,yminv,ymaxv,cmap = 'RdYlBu_r', log_plot=False):
     # Use viridis for absolute maps
